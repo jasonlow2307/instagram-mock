a6447cb142045495093d3255e1e738cfd4d76c95
Jason <jason.lowjx@gmail.com>
2024-12-05 13:20:01 +0800

    modified ip

--- MessagingServer.java
+++ MessagingServer.java
@@ -67,7 +67,7 @@ public class MessagingServer extends UnicastRemoteObject implements MessagingSer
 
     public static void main(String[] args) {
         try {
-            System.setProperty("java.rmi.server.hostname", "18.141.204.33");
+            System.setProperty("java.rmi.server.hostname", "172.31.30.129");
             LocateRegistry.createRegistry(1099);
             MessagingServer server = new MessagingServer();
             Registry registry = LocateRegistry.getRegistry(1099);
